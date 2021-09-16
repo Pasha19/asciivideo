06be3405265570e82b6e8a05d10d095a6ecfeb43
Pasha19 <20466125+Pasha19@users.noreply.github.com>
2021-09-16 23:59:13 +0300

    play txt files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import pathlib
+import time
 
 import cv2
 
@@ -71,27 +73,72 @@ def encode_frame_diff(frame1, frame2):
     return result
 
 
+def read_by_frames(file):
+    while True:
+        read = file.read(7)
+        if not read:
+            break
+        size_to_read = int(read.strip().lstrip("0"))  # 6 + LF
+        frame_data = file.read(size_to_read)
+        yield frame_data
+
+
+def restore_frame(frame):
+    result = ""
+    digits = list(map(str, range(0, 10)))
+    cnt = 0
+    p = frame[0]
+    for i in range(1, len(frame)):
+        c = frame[i]
+        if c in digits:
+            cnt = cnt * 10 + int(c)
+        else:
+            result += p if cnt == 0 else cnt * p
+            p = c
+            cnt = 0
+    return result
+
+
+def restore_frame_diff(frame1, frame2):
+    result = ""
+    for i in range(len(frame2)):
+        c = frame2[i]
+        result += frame1[i] if c == "=" else c
+    return result
+
+
 def main():
     wd = pathlib.Path().resolve()
     image_path = pathlib.Path(wd).parent.parent.resolve()
+    #
+    # video_file = "{0}\\{1}".format(image_path, "Me-at-the-zoo-YouTube.mp4")
+    # video = cv2.VideoCapture(video_file)
+    #
+    # output = open("{0}\\{1}".format(image_path, "result_diff.txt"), "w")
+    # prev_frame = None
+    # width = 100
+    # for img in extract_frames(video):
+    #     frame = img_to_ascii(img, width)
+    #     if prev_frame is None:
+    #         encoded = encode_single_frame(frame)
+    #     else:
+    #         encoded = encode_single_frame(encode_frame_diff(prev_frame, frame))
+    #     output.write("{:06d}\n".format(len(encoded)))
+    #     output.write(encoded)
+    #     prev_frame = frame
+    # output.close()
+    # video.release()
 
-    video_file = "{0}\\{1}".format(image_path, "Me-at-the-zoo-YouTube.mp4")
-    video = cv2.VideoCapture(video_file)
-
-    output = open("{0}\\{1}".format(image_path, "result_diff.txt"), "w")
-    prev_frame = None
-    width = 100
-    for img in extract_frames(video):
-        frame = img_to_ascii(img, width)
-        if prev_frame is None:
-            encoded = encode_single_frame(frame)
-        else:
-            encoded = encode_single_frame(encode_frame_diff(prev_frame, frame))
-        output.write("{:06d}\n".format(len(encoded)))
-        output.write(encoded)
-        prev_frame = frame
-    output.close()
-    video.release()
+    text_path = "{0}\\{1}".format(image_path, "result_diff.txt")
+    frame0 = ""
+    with open(text_path) as f:
+        for frame in read_by_frames(f):
+            os.system('cls')
+            frame = restore_frame(frame)
+            frame = restore_frame_diff(frame0, frame)
+            print(frame)
+            frame0 = frame
+            time.sleep(0.04)
 
 
 main()
